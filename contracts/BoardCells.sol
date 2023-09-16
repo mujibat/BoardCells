6398dd10066ac32f740544f1cbd3b75540287bbf
mujibat <oshikoyadolapo13@gmail.com>
2023-09-16 23:32:35 +0100

    boardcells

---
+++ contracts/BoardCells.sol
@@ -0,0 +1,200 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.19;
+
+// contract Cell{
+//     string id1 = "white";
+//     string id2 = "black"; 
+//     string id3 = "red"; 
+//     int x;
+//     int y;
+//     int m;
+//     string Cell0;
+//     string Cell1;
+//     string Cell2;
+//     string Cell3;
+//     string Cell4;
+//     string Cell5;
+//     string Cell6;
+//     string Cell7;
+//     string Cell8;
+//     string Cell9;
+//     string Cell10;
+//     string Cell11;
+//     string Cell12;
+//     string Cell13;
+//     string Cell14;
+//     string Cell15;
+//     string Cell16;
+//     string Cell17;
+//     string Cell18;
+//     string Cell19;
+//     string Cell20;
+//     string Cell21;
+//     string Cell22;
+//     string Cell23;
+//     string Cell24;
+//     string Cell25;
+//     string Cell26;
+//     string Cell27;
+//     string Cell28;
+//     string Cell29;
+//     string Cell30;
+//     string Cell31;
+//     string Cell32;
+//     string Cell33;
+//     string Cell34;
+
+
+
+//     function loopthruValuesforCells(int x1, int x2, int y1, int y2) external {
+//           x = x2 - x1;
+//         y =  y2 - y1;
+//         m = y / x;
+//          for(int i = 0; i < 35; i++){
+//             if (i == 0){ //0
+//                 Cell0 = id1;
+//                 continue;
+//             }
+//              if (i == 1){ //1
+//                 Cell1 = id1;
+//                  continue;
+//             }
+//              if (i == 2){//2
+//                 Cell2 = id1;
+//                  continue;
+//             }
+//              if (i == 3){//3
+//                 Cell3 = id3;
+//                  continue;
+//             }
+//              if (i == 4){//4
+//                 Cell4 = id2;
+//                  continue;
+//             }
+//              if (i == 5){//5
+//                 Cell5 = id2;
+//                 continue;
+//             }
+//              if (i == 6){//6
+//                 Cell6 = id2;
+//                 continue;
+//             }
+//              if (i == 7){//7
+//                 Cell7 = id1;
+//                 continue;
+//             }
+//              if (i == 8){//8
+//                 Cell8 = id1;
+//                 continue;
+//             }
+//              if (i == 9){//9
+//                 Cell9 = id1;
+//                 continue;
+//             }
+//              if (i == 10){//10
+//                 Cell10 = id3;
+//                 continue;
+//             }
+//              if (i == 11){//11
+//                 Cell11 = id2;
+//                 continue;
+//             }
+//              if (i == 12){//12
+//                 Cell12 = id2;
+//                 continue;
+//             }
+//              if (i == 13){//13
+//                 Cell13 = id2;
+//                 continue;
+//             }
+//              if (i == 14){//14
+//                 Cell14 = id1;
+//                 continue;
+//             }
+//              if (i == 15){//15
+//                 Cell15 = id1;
+//                 continue;
+//             }
+//              if (i == 16){//16
+//                 Cell16 = id1;
+//                 continue;
+//             }
+//              if (i == 17){//17
+//                 Cell17 = id3;
+//                 continue;
+//             }
+//              if (i == 18){//18
+//                 Cell18 = id2;
+//                 continue;
+//             }
+//              if (i == 19){//19
+//                 Cell19 = id2;
+//                 continue;
+//             }
+//              if (i == 20){//20
+//                 Cell20 = id2;
+//                 continue;
+//             }
+//              if (i == 21){//21
+//                 Cell21 = id1;
+//                 continue;
+//             }
+//              if (i == 22){//22
+//                 Cell22 = id1;
+//                 continue;
+//             }
+//             if (i == 23){//23
+//                 Cell23 = id3;
+//                 continue;
+//             }
+//             if (i == 24){//24
+//                 Cell24 = id3;
+//                 continue;
+//             }
+//             if (i == 25){//25
+//                 Cell25 = id3;
+//                 continue;
+//             }
+//             if (i == 26){//26
+//                 Cell26 = id2;
+//                 continue;
+//             }
+//             if (i == 27){//27
+//                 Cell27 = id2;
+//                 continue;
+//             }
+//             if (i == 28){//28
+//                 Cell28 = id3;
+//                 continue;
+//             }
+//             if (i == 29){//29
+//                 Cell29 = id2;
+//                 continue;
+//             }
+//             if (i == 30){//30
+//                 Cell30 = id3;
+//                 continue;
+//             }
+//             if (i == 31){//31
+//                 Cell31 = id3;
+//                 continue;
+//             }
+//             if (i == 32){//32
+//                 Cell32 = id3;
+//                 continue;
+//             }
+//             if (i == 33){//33
+//                 Cell33 = id3;
+//                 continue;
+//             }
+//             if (i == 34){//34
+//                 Cell34 = id3;
+//                 continue;
+//             }
+//             // else {//35
+//             //     revert("Not a valid number");
+//             // }
+           
+//         }
+//     }
+// }  
---
+++ contracts/cellboard.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract Cell{
+    string[] ids = ["white", "black", "red"];
+    uint256 private nonce = 0;
+
+    function getRandomValue() internal returns (uint256) {
+        uint256 random = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    // block.difficulty,
+                    nonce++
+                )
+            )
+        );
+
+        // Scale the random number to be between 0 and 2
+        return random % 3;
+    }
+
+    mapping(int => string) public _cells;
+
+    function getColorforCells(uint x1, uint x2, uint y1, uint y2) public pure returns(uint){
+        uint x = x2 - x1;
+        uint y =  y2 - y1;
+        uint m = y / x;
+        // console.log(m, "kkjkjkkjk");
+        //  return _cells[m];
+        return m;
+    }
+    constructor() {
+      for(int i = 0; i < 35; i++){
+            _cells[i] = ids[getRandomValue()];
+        }
+    }
+    
+}  
